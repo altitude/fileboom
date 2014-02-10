7e6cdb9191cca92b9cff05858100e038e2694a0b
altitude-labs <salaun.clement@gmail.com>
2014-02-10 23:42:11 +0100

    adding DELETE /token

--- routes/deleteToken.js
+++ routes/deleteToken.js
@@ -2,4 +2,18 @@ var	redis   = require("redis").createClient();
 
 module.exports = function(req, res){
 	
+	var api_id = req.body.api_id;
+	var api_key = req.body.api_key;
+	var token = req.body.token;
+
+	if(api_id != config.api_id || api_key != config.api_key){
+		res.writeHead(403);
+		res.end("Sorry, you can't do this.");
+	}
+
+	redis.del(token, function(){
+		res.writeHead(200);
+		res.end("Token removed");
+	});
+
 }
